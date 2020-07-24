0a8d09b553f51b9e9f77200ff2d06e5ccede113e
tu tv <tutrinhtvt97@gmail.com>
2020-07-24 10:06:46 +0700

    Updates

---
+++ precache-manifest.a187a1bd439b4298c294a5bb12def86c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b922f0f28183cf5341cdd11739e4a54",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "c500e5d4607f17406db7",
+    "url": "/react/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "4411947d4e09188d23b6",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c500e5d4607f17406db7",
+    "url": "/react/static/js/2.d034081c.chunk.js"
+  },
+  {
+    "revision": "2c9c8e3d25851920df3b39d1327b1a2f",
+    "url": "/react/static/js/2.d034081c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4411947d4e09188d23b6",
+    "url": "/react/static/js/main.b4315528.chunk.js"
+  },
+  {
+    "revision": "e13f72c0a5b2117a7fa1",
+    "url": "/react/static/js/runtime-main.da8bee19.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/react/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
